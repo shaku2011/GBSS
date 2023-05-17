9ce54cc75772c564653cbc02fc9ef1756c883967
K Takahashi <122062733+K0ki116@users.noreply.github.com>
2023-05-17 14:07:06 +0900

    Update gbss_3_fill.sh

--- NODDI/gbss_3_fill.sh
+++ NODDI/gbss_3_fill.sh
@@ -69,5 +69,5 @@ fslmaths bin_mean_ODI.nii.gz -thr 0.995 mask_ODI
 
 
 #randomise script changed a little
-randomise -i all_fIC_filled.nii.gz -o gbss_fic -m mask.nii.gz -d plus.mat -t plus.con -n 10000 --T2 -V -x --uncorrp -R
-randomise -i all_ODI_filled.nii.gz -o gbss_ODI -m mask_ODI.nii.gz -d plus.mat -t plus.con -n 10000 --T2 -V -x --uncorrp -R
+#randomise -i all_fIC_filled.nii.gz -o gbss_fic -m mask.nii.gz -d plus.mat -t plus.con -n 10000 --T2 -V -x --uncorrp -R
+#randomise -i all_ODI_filled.nii.gz -o gbss_ODI -m mask_ODI.nii.gz -d plus.mat -t plus.con -n 10000 --T2 -V -x --uncorrp -R
